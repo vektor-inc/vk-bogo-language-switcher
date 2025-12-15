65261c9a997c86e194ada04089dd8f4e3970f40f
kurudrive <kurudrive@gmail.com>
2025-12-16 00:17:46 +0900

    [ Change version ] 0.0.2

--- vk-bogo-language-switcher.php
+++ vk-bogo-language-switcher.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: VK Bogo Language Switcher
  * Description: Bogo の言語切替えメニューにスタイルオプションを追加します。
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Vektor,Inc.
  * Text Domain: vk-bogo-language-switcher
  * Domain Path: /languages
